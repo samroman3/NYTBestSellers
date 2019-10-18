d811c00bbeffd4a51ffb144133448122015780b4
hildyabreu114 <hildyabreu@pursuit.org>
2019-10-18 15:40:55 -0400

    add a file and the models for APIS

--- NYTBestsellers/NYTBestsellers/Models/Google.swift
+++ NYTBestsellers/NYTBestsellers/Models/Google.swift
@@ -7,3 +7,107 @@
 //
 
 import Foundation
+
+// MARK: - GoogleInfo
+struct GoogleInfo: Codable {
+    let kind: String?
+    let totalItems: Int?
+    let items: [Item]?
+    
+    static func getForecastFromData(data: Data) throws -> GoogleInfo? {
+        do {
+            let info = try JSONDecoder().decode(GoogleInfo.self, from: data)
+            return info
+
+        } catch {
+            print(error)
+            return nil
+        }
+
+    }
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let kind, id, etag: String?
+    let selfLink: String?
+    let volumeInfo: VolumeInfo?
+    let saleInfo: SaleInfo?
+    let accessInfo: AccessInfo?
+    let searchInfo: SearchInfo?
+}
+
+// MARK: - AccessInfo
+struct AccessInfo: Codable {
+    let country, viewability: String?
+    let embeddable, publicDomain: Bool?
+    let textToSpeechPermission: String?
+    let epub, pdf: Epub?
+    let webReaderLink: String?
+    let accessViewStatus: String?
+    let quoteSharingAllowed: Bool?
+}
+
+// MARK: - Epub
+struct Epub: Codable {
+    let isAvailable: Bool?
+}
+
+// MARK: - SaleInfo
+struct SaleInfo: Codable {
+    let country, saleability: String?
+    let isEbook: Bool?
+}
+
+// MARK: - SearchInfo
+struct SearchInfo: Codable {
+    let textSnippet: String?
+}
+
+// MARK: - VolumeInfo
+struct VolumeInfo: Codable {
+    let title, subtitle: String?
+    let authors: [String]?
+    let publisher, publishedDate, volumeInfoDescription: String?
+    let industryIdentifiers: [IndustryIdentifier]?
+    let readingModes: ReadingModes?
+    let pageCount: Int?
+    let printType: String?
+    let categories: [String]?
+    let averageRating: Double?
+    let ratingsCount: Int?
+    let maturityRating: String?
+    let allowAnonLogging: Bool?
+    let contentVersion: String?
+    let panelizationSummary: PanelizationSummary?
+    let imageLinks: ImageLinks?
+    let language: String?
+    let previewLink, infoLink: String?
+    let canonicalVolumeLink: String?
+
+    enum CodingKeys: String, CodingKey {
+        case title, subtitle, authors, publisher, publishedDate
+        case volumeInfoDescription = "description"
+        case industryIdentifiers, readingModes, pageCount, printType, categories, averageRating, ratingsCount, maturityRating, allowAnonLogging, contentVersion, panelizationSummary, imageLinks, language, previewLink, infoLink, canonicalVolumeLink
+    }
+}
+
+// MARK: - ImageLinks
+struct ImageLinks: Codable {
+    let smallThumbnail, thumbnail: String?
+}
+
+// MARK: - IndustryIdentifier
+struct IndustryIdentifier: Codable {
+    let type, identifier: String?
+}
+
+// MARK: - PanelizationSummary
+struct PanelizationSummary: Codable {
+    let containsEpubBubbles, containsImageBubbles: Bool?
+}
+
+// MARK: - ReadingModes
+struct ReadingModes: Codable {
+    let text, image: Bool?
+}
