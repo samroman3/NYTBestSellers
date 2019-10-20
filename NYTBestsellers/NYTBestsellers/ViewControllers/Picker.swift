59b3f1243ebcf6366917987428e875020bec16d6
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-20 15:19:10 -0400

    makes picker() auto load the categories

--- NYTBestsellers/NYTBestsellers/ViewControllers/Picker.swift
+++ NYTBestsellers/NYTBestsellers/ViewControllers/Picker.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class Picker: UIPickerView {
+    var categories = [Hit]() {
+        didSet {
+            self.reloadAllComponents()
+        }
+    }
 
     override init(frame: CGRect) {
         super.init(frame: CGRect(x: 0, y: 0, width: 200, height: 150))
@@ -16,9 +21,43 @@ class Picker: UIPickerView {
         layer.borderWidth = 2
         layer.borderColor = UIColor.purple.cgColor
         layer.cornerRadius = 30
+        setPickerDelegates()
+        loadCatagories()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    //MARK: - Functions
+    private func loadCatagories() {
+        NYTAPIClient.shared.getCategory { (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let categoriesFromJSON):
+                if let hits = categoriesFromJSON.results {
+                    self.categories = hits
+                }
+            }
+        }
+    }
+    private func setPickerDelegates() {
+        delegate = self
+        dataSource = self
+    }
+}
+
+//MARK: Extension
+extension Picker: UIPickerViewDataSource,UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row].listName
+    }
+    
 }
