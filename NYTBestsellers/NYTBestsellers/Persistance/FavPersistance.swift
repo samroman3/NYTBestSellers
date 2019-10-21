ff63f109ff699a6cf760cf9029bdb1c604e4f436
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-21 11:52:05 -0400

    adds to persistance

--- NYTBestsellers/NYTBestsellers/Persistance/FavPersistance.swift
+++ NYTBestsellers/NYTBestsellers/Persistance/FavPersistance.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+struct FavPersistenceHelper {
+    
+    private static var pictures = [Favorite]()
+    
+    static let manager = FavPersistenceHelper()
+    func save(newPhoto: Favorite) throws {
+        try persistenceHelper.save(newElement: newPhoto)
+    }
+    func getPhoto() throws -> [Favorite] {
+        return try persistenceHelper.getObjects()
+    }
+    
+    func editPhoto(element: Favorite, index: Int) throws {
+        return try persistenceHelper.editAtIndex(editedElement: element, index: index)
+    }
+    
+    //TODO: find way to delete picture based on sender tag in collectionview cell
+    func delete(favArr: [Favorite], index: Int) throws {
+        return try persistenceHelper.deleteAtIndex(newArray: favArr, index: index)
+        
+}
+    
+    private let persistenceHelper = PersistenceHelper<Favorite>(fileName: "favBooks.plist")
+    private init() {}
+}
--- NYTBestsellers/NYTBestsellers/Persistance/UserDefaultWrapper.swift
+++ NYTBestsellers/NYTBestsellers/Persistance/UserDefaultWrapper.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+class UserDefaultWrapper {
+    
+    private init () {}
+    
+    static let manager = UserDefaultWrapper()
+    
+    //MARK: Keys
+    private let categoryRowKey = "category"
+    private let categoryNameKey = "name"
+    
+    
+    //MARK: Sets
+    func set(value: Int) {
+        print("saved: \(value)")
+        UserDefaults.standard.set(value, forKey: categoryRowKey)
+    }
+    func set(name: String) {
+        UserDefaults.standard.set(name, forKey: categoryNameKey)
+    }
+    
+    //MARK: Gets
+    func getCategoryRow() -> Int? {
+        if let row = UserDefaults.standard.value(forKey: categoryRowKey) as? Int {
+            return row
+        }
+        return nil
+    }
+    func getCategoryName() -> String? {
+        if let name = UserDefaults.standard.value(forKey: categoryNameKey) as? String {
+            return name
+        }
+        return nil
+    }
+    
+}
